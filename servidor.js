184746335e1c2b8e070cfac55016db9b2eda3094
GABRIEL MARTIN PADILHA CARADA <gabriel_carada@estudante.sesisenai.org.br>
2025-08-13 12:32:07 -0300

    feito

--- servidor.js
+++ servidor.js
@@ -2,19 +2,37 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const dados1 = require('./dados1.json');
+const dados2 = require('./dados2.json');
+const dados3 = require('./dados3.json');
 
 app.use(cors());
 app.use(express.json());
 
-//Criar um endpoint para retornar pesquisa
-app.post('/buscarPessoas', (req, res) =>{
-    const {nome} = req.body;
+app.post('/buscarPessoas', (req, res) => {
+    const { nome } = req.body;
     const resultado = dados1.filter(pessoa =>
         pessoa.nome.toLowerCase().includes(nome.toLowerCase())
     );
     return res.json(resultado);
 });
 
-app.listen(3000, () => {
-    console.log('Servidor executando')
+app.post('/buscarCarros', (req, res) => {
+    const { marca } = req.body;
+    const resultado2 = dados2.filter(carro =>
+        carro.marca.toLowerCase().includes(marca.toLowerCase())
+    );
+    return res.json(resultado2);
 });
+
+app.post('/buscarFilmes', (req, res) => {
+    const { titulo } = req.body;
+    const resultado3 = dados3.filter(filme =>
+        filme.titulo.toLowerCase().includes(titulo.toLowerCase())
+    );
+    return res.json(resultado3);
+});
+
+app.listen(3000, () => {
+    console.log('Servidor executando');
+});
+
